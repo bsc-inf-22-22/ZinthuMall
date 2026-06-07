1ab27b427dd2cf478fbc8d6d3f1ea4991954e31e
bsc-com-24-23 <bsc-com-24-23@unima.ac.mw>
2026-06-07 23:32:04 +0200

    Admin auth(Register and login) with endpoints tested

---
+++ src/admin-backend/src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'Luhanga@21',
+      database: 'Admin',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    AuthModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
